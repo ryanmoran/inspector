19d5e02b31cf94b95aef375dd57b8b5dded75680
Ryan Moran <rmoran@pivotal.io>
2016-12-14 20:15:29 -0800

    First passing test

---
+++ acceptance/help_test.go
@@ -0,0 +1,21 @@
+package acceptance
+
+import (
+	"os/exec"
+
+	"github.com/onsi/gomega/gexec"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("help", func() {
+	It("prints the usage", func() {
+		command := exec.Command(pathToMain, "help")
+
+		session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+
+		Eventually(session).Should(gexec.Exit(0))
+	})
+})
